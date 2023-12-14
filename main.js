ddf86264360f1ffc6c7c0d4187ec5defe8718fc8
100549569 <147156946+100549569@users.noreply.github.com>
2023-12-14 22:18:13 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize the Minecraft widget when the DOM is ready
+    initializeMinecraftWidget();
+});
+
+function initializeMinecraftWidget() {
+    // Your initialization logic for the Minecraft widget goes here
+    // You can call functions from minecraft-widget.js or add your initialization code directly.
+}
